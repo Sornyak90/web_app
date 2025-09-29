fcf0ca6dbdd3e68b46d6b57ee6e4de31260d5bc9
Your Name <you@example.com>
2025-09-29 15:18:36 +0500

    app

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def top():
+    return "top here"
+
+@app.get("/echo/{thing}")
+def echo(thing):
+    return f"echoing {thing}"
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
